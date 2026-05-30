94b6939b22a054db3ddfe5ca9d47b36646528be0
Sarang681 <sarang0608.desai@gmail.com>
2026-05-30 23:52:41 +0530

    feat: add logic to shorten and un-shorten the URLs

--- src/db.rs
+++ src/db.rs
@@ -7,3 +7,33 @@ pub async fn connection_pool(url: &str) -> Pool<Postgres> {
         .await
         .expect("Failed to create DB connection pool")
 }
+
+pub async fn add_url(url: &str, pool: &Pool<Postgres>) -> u64 {
+    sqlx::query!(
+        r#"
+    INSERT INTO urls (long_url)
+        VALUES ($1)
+        RETURNING id
+    "#,
+        url
+    )
+    .fetch_one(pool)
+    .await
+    .unwrap()
+    .id as u64
+}
+
+pub async fn fetch_url(id: u64, pool: &Pool<Postgres>) -> String {
+    sqlx::query!(
+        r#"
+    SELECT long_url
+    FROM urls
+    WHERE id = ($1)
+    "#,
+        id as i64
+    )
+    .fetch_one(pool)
+    .await
+    .unwrap()
+    .long_url
+}
--- src/routes/urls.rs
+++ src/routes/urls.rs
@@ -1,15 +1,20 @@
 use axum::{
     Json, Router,
-    extract::Path,
+    extract::{Path, State},
     routing::{get, post},
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-use crate::AppState;
+use crate::{AppState, db, encode};
 
 #[derive(Deserialize)]
 struct ShortenRequest {
     long_url: String,
+}
+
+#[derive(Serialize)]
+struct ShortenResponse {
+    short_code: String,
 }
 
 pub fn router() -> Router<AppState> {
@@ -18,13 +23,24 @@ pub fn router() -> Router<AppState> {
         .route("/{code}", get(handle_redirect_from_short_code))
 }
 
-async fn handle_shorten_url(Json(request): Json<ShortenRequest>) -> String {
-    format!("Shortening the following url :: {}", request.long_url)
+async fn handle_shorten_url(
+    State(state): State<AppState>,
+    Json(request): Json<ShortenRequest>,
+) -> Json<ShortenResponse> {
+    let db_pool = state.conn_pool;
+    let id = db::add_url(&request.long_url, &db_pool).await;
+    let short_code = encode::encode(id);
+
+    Json(ShortenResponse { short_code })
 }
 
-async fn handle_redirect_from_short_code(Path(short_code): Path<String>) -> String {
-    format!(
-        "Redirecting to the long url from this url :: {}",
-        short_code
-    )
+async fn handle_redirect_from_short_code(
+    State(state): State<AppState>,
+    Path(short_code): Path<String>,
+) -> String {
+    let db_pool = state.conn_pool;
+
+    let decoded_id = encode::decode(&short_code).unwrap();
+
+    db::fetch_url(decoded_id, &db_pool).await
 }
