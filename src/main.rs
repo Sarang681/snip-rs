6fbeb03cbaeca36bb4718a6a861e3638af3c46ee
Sarang681 <sarang0608.desai@gmail.com>
2026-05-30 17:39:45 +0530

    feature: configure initial axum setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let router: Router<()> = Router::new();
+
+    // bind to localhost port 8080 for now
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+
+    axum::serve(listener, router).await.unwrap();
 }
