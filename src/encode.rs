6ded9b25a2d6750fc43c9f1a2fd12039c816e856
Sarang681 <sarang0608.desai@gmail.com>
2026-05-30 21:16:14 +0530

    chore: add unit tests for encoding and decoding

--- src/encode.rs
+++ src/encode.rs
@@ -25,3 +25,40 @@ pub fn decode(encoded_str: &str) -> Option<u64> {
         Some(acc * 62 + pos)
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encode_id_success() {
+        let id = 12765;
+        assert_eq!(encode(id), "3Jt");
+    }
+
+    #[test]
+    fn test_decode_string_success() {
+        let code = "3Jt";
+        let decoded_string = decode(code);
+
+        assert_eq!(decoded_string, Some(12765));
+    }
+
+    #[test]
+    fn test_decode_invalid_string_failure() {
+        let code = "*abc";
+        let res = decode(code);
+
+        assert!(res.is_none());
+    }
+
+    #[test]
+    fn test_round_trip() {
+        let id = 12765;
+
+        let encoded_string = encode(id);
+        let decoded_string = decode(&encoded_string);
+
+        assert_eq!(decoded_string, Some(id));
+    }
+}
